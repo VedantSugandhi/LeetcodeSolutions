18b025d867f21b672370824993643952dfc4595d
Vedant09 <vedantsugandhi95@gmail.com>
2025-06-18 21:57:52 -0400

    Path from Source to Target

---
+++ src/DFS/pathsSourceTarget.java
@@ -0,0 +1,24 @@
+package DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pathsSourceTarget {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            List<List<Integer>> result = new ArrayList<>();
+
+            calculatePath(graph,result, new ArrayList<>(),0);
+
+            return result;
+        }
+        public void calculatePath(int[][] graph, List<List<Integer>> result, List<Integer> path,int curr){
+            path.add(curr);
+            if(curr == graph.length-1)
+                result.add(new ArrayList<>(path));
+            else{
+                for(int node : graph[curr])
+                    calculatePath(graph, result, path,node);
+            }
+            path.remove(path.size()-1);
+        }
+}
