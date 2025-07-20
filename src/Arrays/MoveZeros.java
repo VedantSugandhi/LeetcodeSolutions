830f516813b5388792359889b8cfee790ce594d4
Vedant09 <vedantsugandhi95@gmail.com>
2025-07-19 21:23:52 -0400

    Array easy questions

---
+++ src/Arrays/MoveZeros.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for(int num: nums){
+            if(num!=0)
+                nums[insertPos++] = num;
+        }
+        while(insertPos<nums.length)
+            nums[insertPos++] = 0;
+    }
+}
