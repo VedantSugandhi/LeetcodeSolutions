f5083e6f9e24fc56ca384d06f146c6e04dd645b4
Vedant Sugandhi <54495810+VedantSugandhi@users.noreply.github.com>
2026-04-29 17:09:58 -0400

    0817 (#11)
    * best Time To buy and sell stock
    
    * included 2 as well.
    
    * feat: implement various array, sorting, and searching algorithms for LeetCode practice
    
    * feat: add implementations for common searching, sorting, and array algorithms

--- src/Arrays/BestTimeToBuyAndSellStock.java
+++ src/Arrays/BestTimeToBuyAndSellStock.java
@@ -11,13 +11,20 @@ public class BestTimeToBuyAndSellStock {
         }
         return maxDiff;
     }
-    //Best time to buy and sell stock 2
+
+    // Best time to buy and sell stock 2
     public int maxProfit2(int[] prices) {
         int profit = 0;
-        for(int i = 1; i<prices.length;i++){
-            if(prices[i] > prices[i-1])
-                profit += prices[i] - prices[i-1];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
         }
         return profit;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(bestTimeToBuyAndSellStock.maxProfit(prices));
     }
 }
